f2deb3d2803d027fb16b41f18424cada7e5f2fd0
Wendmagegn Kebede <wendmagegn.b@gmail.com>
2022-10-26 21:28:39 -0500

    team stat layout update

--- app/src/main/java/com/example/fragments/ui/teamStat/TeamStatActivity.kt
+++ app/src/main/java/com/example/fragments/ui/teamStat/TeamStatActivity.kt
@@ -4,18 +4,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
+import com.bumptech.glide.Glide
 import com.example.fragments.databinding.ActivityTeamStatBinding
+import kotlinx.android.synthetic.main.activity_team_stat.*
+import kotlinx.android.synthetic.main.item_standings.view.*
 
 
 class TeamStatActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTeamStatBinding
     private var adapter : TeamStatAdapter? = null
+    private var homeTeamName:String? = null
+    private var awayTeamName:String? = null
+    private var homeTeamScore:Int = 0
+    private  var awayTeamScore:Int  = 0
+    private  var homeTeamLogo:String? = null
+    private  var awayTeamLogo :String?= null
+    private  var fixtureId:Int  = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTeamStatBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+
 
         initViewModel()
 
@@ -26,6 +39,21 @@ class TeamStatActivity : AppCompatActivity() {
         val viewModel = TeamStatActivityViewModel()
         viewModel.makeApiCall()
         viewModel.getResponse().observe(this) {
+            homeTeamName = intent?.getStringExtra("homeTeamName")
+            awayTeamName = intent?.getStringExtra("awayTeamName")
+            homeTeamScore = intent.getIntExtra("homeTeamScore",0)
+            awayTeamScore = intent.getIntExtra("awayTeamScore",0)
+            homeTeamLogo = intent?.getStringExtra("homeTeamName")
+            awayTeamLogo =intent?.getStringExtra("homeTeamName")
+            fixtureId = intent.getIntExtra("fixtureId",0)
+
+            team1Txt.text = homeTeamName
+            team2Txt.text = awayTeamName
+            team1GameTxt.text = homeTeamScore.toString()
+            team2GoalTxt.text = awayTeamScore.toString()
+
+            Glide.with(this).load(homeTeamLogo).into(team1Img)
+            Glide.with(this).load(homeTeamLogo).into(team2Img)
             adapter = TeamStatAdapter(it)
 
             binding!!.rvStandings.layoutManager = GridLayoutManager(this, 1)
