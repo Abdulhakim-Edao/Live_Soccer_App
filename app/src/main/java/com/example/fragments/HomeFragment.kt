911468fed0fda71da605a48f310d63a45a208793
Abdulhakim Edao <abdulhakim.edao1@gmail.com>
2022-10-26 19:33:54 -0500

    Bug Fix

--- app/src/main/java/com/example/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/fragments/HomeFragment.kt
@@ -5,8 +5,6 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import com.example.fragments.ui.fixtures.FixturesActivity
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
--- app/src/main/java/com/example/fragments/ui/fixtures/Days.kt
+++
@@ -1,6 +0,0 @@
-package com.example.fragments.ui.fixtures
-
-data class Days(var ChiledItemTitle: String?) {
-
-
-}
