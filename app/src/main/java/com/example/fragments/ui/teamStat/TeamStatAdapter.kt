fef57f65115943fd6ef67b1c4d1544e91eaa0dd3
Wendmagegn Kebede <wendmagegn.b@gmail.com>
2022-10-27 11:43:10 -0500

    Final Commit Api Bug fix 1

--- app/src/main/java/com/example/fragments/ui/teamStat/TeamStatAdapter.kt
+++ app/src/main/java/com/example/fragments/ui/teamStat/TeamStatAdapter.kt
@@ -21,9 +21,9 @@ class TeamStatAdapter(res: TeamStat) : RecyclerView.Adapter<TeamStatAdapter.MyVi
     class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         fun bind(response1: List<Statistic>, response2: List<Statistic>, response: Team) {
-            itemView.rank.text = response1[adapterPosition].value
-            itemView.teamName.text = response1[adapterPosition].type
-            itemView.goalsDiff.text = response2[adapterPosition].value
+            itemView.team1Stat.text = response1[adapterPosition].value
+            itemView.stats.text = response1[adapterPosition].type
+            itemView.team2Stat.text = response2[adapterPosition].value
 
         }
 
